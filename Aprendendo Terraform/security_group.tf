960db4288b0837c492f026aaeab4c11cd13d00cf
Diego <diegoneto@hotmail.com.br>
2021-09-10 04:58:59 -0300

    variaveis

--- Aprendendo Terraform/security_group.tf
+++ Aprendendo Terraform/security_group.tf
@@ -1,15 +1,15 @@
 resource "aws_security_group" "acesso_geral" {
   name        = "acesso_geral"
   description = "Allow TLS inbound traffic"
-
+  vpc_id      = aws_vpc.main.id
   ingress = [
     {
       description      = "TLS from VPC"
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      #cidr_blocks      = [aws_vpc.main.cidr_block]
-      #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+      cidr_blocks      = [aws_vpc.main.cidr_block]
+      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
     }
   ]
 
@@ -18,8 +18,8 @@ resource "aws_security_group" "acesso_geral" {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
-      #cidr_blocks      = ["0.0.0.0/0"]
-      #ipv6_cidr_blocks = ["::/0"]
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 
