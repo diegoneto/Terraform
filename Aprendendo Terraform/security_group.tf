fa8b3cf93a2e2b8c456eaefcced7cd413f0258b6
Diego <diegoneto@hotmail.com.br>
2021-09-10 05:58:25 -0300

    hghghgh

--- Aprendendo Terraform/security_group.tf
+++ Aprendendo Terraform/security_group.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "acesso_geral" {
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+      cidr_blocks      = [acesso_geral.main.cidr_block]
+      ipv6_cidr_blocks = [acesso_geral.main.ipv6_cidr_block]
     }
   ]
 
