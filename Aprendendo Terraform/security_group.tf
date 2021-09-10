f67a872148fa1f0a15b1eca09aee4608d015da60
Diego <diegoneto@hotmail.com.br>
2021-09-10 06:30:58 -0300

    nn

--- Aprendendo Terraform/security_group.tf
+++ Aprendendo Terraform/security_group.tf
@@ -24,6 +24,7 @@ resource "aws_security_group" "acesso_geral" {
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
+      description      = ""
       prefix_list_ids =[]
       security_groups =[]
       self            = false
