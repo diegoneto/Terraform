6507ee1ce494ef40880228e4779555692bae563a
Diego <diegoneto@hotmail.com.br>
2021-11-04 00:25:44 -0300

    fds

--- Aprendendo Terraform/vars.tf
+++ Aprendendo Terraform/vars.tf
@@ -1,8 +1,8 @@
 variable "amis" {
     default = {
       "us-east-1-ubuntu18" = "ami-04a5b8dd2ee07bb23"
-      "us-east-1-ubuntu18" = "ami-ami-04a5b8dd2ee07bb23"
-      "us-east-1-ubuntu18" = "ami-ami-04a5b8dd2ee07bb23"
+      "us-east-1-ubuntu18" = "ami-04a5b8dd2ee07bb23"
+      "us-east-1-ubuntu18" = "ami-04a5b8dd2ee07bb23"
     }   
 }
 
